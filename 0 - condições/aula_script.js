338d0113c3e6d58dd157d9ba4a34dcfcf1f678db
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-10 09:48:48 -0300

    Vamo q Vamo

---
+++ 0 - condições/aula_script.js
@@ -0,0 +1,16 @@
+const nome = 'Felinalista'
+
+
+// Teste 1
+let resultado = (nome === 'Ramon') ? 'Lindo Nome' : 'Nome Horrivel'
+console.log(resultado)
+
+
+// Teste 2
+let resultado2 = (nome === 'Levi') ? 'Que Nome Exotico' : 'Que Nome Simples'
+console.log(resultado2)
+
+
+// Teste 3
+let resultado3 = (nome.length < 5) ? 'Nome Pequeno' : 'Nome Relativamente Grande'
+console.log(resultado3)
---
+++ 999 - pequenos-desafios/2 - velocidade/script.js
@@ -0,0 +1,19 @@
+let car1 = prompt('Nome Do Primeiro Veiculo')
+let velocidadeCar1 = prompt('Velocindade do Primeiro Veiculo')
+
+let car2 = prompt('Nome Do Segundo Veiculo')
+let velocidadeCar2 = prompt('Velocindade do Segundo Veiculo')
+
+
+
+if (velocidadeCar1 > velocidadeCar2) {
+    alert(car1)
+}
+else {
+    if (velocidadeCar1 === velocidadeCar2) {
+        alert('Mesma Velociadade')
+    }
+    else {
+        alert(car2)
+    }
+}
