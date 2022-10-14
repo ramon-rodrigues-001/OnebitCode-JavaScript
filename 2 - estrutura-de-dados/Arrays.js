d9e67ca1a195c236da7f70d237b61b122f639d08
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-14 18:41:30 -0300

    Não é o fim

--- 2 - estrutura-de-dados/Arrays.js
+++ 2 - estrutura-de-dados/Arrays.js
@@ -19,3 +19,13 @@ console.log(array_1)
 // Revovendo do inicio (Fila)
 array_1.shift()
 console.log(array_1)
+
+// Verificando se existe o valor dentro do Array
+console.log(array_1.includes('Ramon'))
+
+// Verificando o indice onde esta o valor
+console.log(array_1.indexOf(18))
+
+
+// === Copiando recortando e referencia ===
+// ????
