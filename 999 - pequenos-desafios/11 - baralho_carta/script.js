8d8a67f24f4503d10bfbdb80cf8dfadf7dba66ff
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-15 17:30:28 -0300

    ainda não

---
+++ 999 - pequenos-desafios/11 - baralho_carta/script.js
@@ -0,0 +1,13 @@
+let array = ['3 - Copas', '2 - Ouros', '6 - Espadas', 'Coringa']
+
+let escolha = prompt("==== BARALHO DE CARTAS ==== \n3 - Copas \n2 - Ouros \n6 - Espadas \nCoringa \n\n[1] _ COMPRAR \n[2] _ PUXAR \n[3] _ SAIR")
+
+if (escolha === '1') {
+    let newcard = 
+}
+if (escolha === '2') {
+    
+}
+if (escolha === '3') {
+    alert('Encerando')
+}
