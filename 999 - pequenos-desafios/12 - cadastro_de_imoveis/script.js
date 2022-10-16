54b5ac018b9b2592aaab06bf7dd958e8cc050823
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-16 19:30:06 -0300

    não terminado

--- 999 - pequenos-desafios/12 - cadastro_de_imoveis/script.js
+++ 999 - pequenos-desafios/12 - cadastro_de_imoveis/script.js
@@ -1,8 +1,10 @@
 let arr = []
 let obj = {}
+let organizado = ''
 let choice;
 
 do {
+    organicao()
     choice = prompt(
         `====== CADASTRO DE IMOVEIS ======
         \nIMOVEIS CADASTRADOS: ${arr.length}
@@ -11,6 +13,20 @@ do {
 
     if (choice === '1') {
         obj.nome = prompt('Informe o nome do proprietário')
+        obj.quartos = prompt('Quantos quartos posuem no imovel: ')
+        obj.banheiros = prompt('Quantos banheiros posuem no imovel: ')
+        obj.garagem = prompt('O imovel posue garagem: [Sim/Nao]')
+
+        arr.push(obj)
+    }
+    else if (choice === '2') {
+        alert(`1º Imovel \n${organizado}`)
     }
 
 } while(choice !== '3')
+
+function organicao() {
+    for (let i = 0; i<arr.length; i++) {
+        organizado += `${arr[i]} \n`
+    }
+}
