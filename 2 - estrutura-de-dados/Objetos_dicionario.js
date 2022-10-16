6daf7b6d5d088e1b12dd43e1ff3dee5f10c0faa1
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-16 13:46:22 -0300

    aula objetos

---
+++ 2 - estrutura-de-dados/Objetos_dicionario.js
@@ -0,0 +1,14 @@
+// Primeira ilustração
+let nome = 'Ramon'
+
+let objeto = {}
+objeto.Ramon = 'Ramon 2.0'
+
+console.log(objeto[nome])
+console.log(nome)
+
+
+// Segunda ilustração
+let dicionario = {nome: 'Ramon', idade: 18, sexo: 'masculino'}
+console.log(dicionario)
+console.log(dicionario.nome)
