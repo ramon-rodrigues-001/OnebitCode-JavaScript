82300780d446d5b7131e5d2319a0b3739e88de04
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-16 08:57:42 -0300

    fim exercício baralho

--- 999 - pequenos-desafios/11 - baralho_carta/script.js
+++ 999 - pequenos-desafios/11 - baralho_carta/script.js
@@ -1,15 +1,39 @@
-let array = ['3 - Copas', '2 - Ouros', '6 - Espadas', 'Coringa']
+let array = ['Ass - Copas', 'Coringa']
 
-let escolha = prompt("==== BARALHO DE CARTAS ==== \n3 - Copas \n2 - Ouros \n6 - Espadas \nCoringa \n\n[1] _ COMPRAR \n[2] _ PUXAR \n[3] _ SAIR")
 
-if (escolha === '1') {
-    let newcard = 
-}
-if (escolha === '2') {
+let escolha = '0'
+while (true) {
+    let organizado = ''
+    function organizacao() {
+        organizado = ''
+        for (let i = 0; i < array.length; i++) {
+            organizado += array[i] + '\n'
+        }
+        apresentação()
+    }
 
+    function apresentação() {
+        escolha = prompt(`==== BARALHO DE CARTAS ==== \n${organizado} \n\n[1] _ COMPRAR \n[2] _ PUXAR \n[3] _ SAIR`)
+
+        // comprar carta
+        if (escolha === '1') {
+            let newCard = prompt('===Name the Card=== \n')
+            array.unshift(newCard)
+            organizacao()
+        }
+
+        // Jogar carta
+        else if (escolha === '2') {
+            let popCard = array.shift()
+            alert(popCard)
+            organizacao()
+        }
+    }
     
-    
-}
-if (escolha === '3') {
-    alert('Encerando')
+    if (escolha === '3') {
+        alert('FIM')
+        break
+    }
+
+    organizacao()
 }
