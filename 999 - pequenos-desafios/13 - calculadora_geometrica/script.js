446bcec7abab6f3f648df44a421583a8264cc411
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-17 16:15:08 -0300

    inicio do exercício calculo geometrico

---
+++ 999 - pequenos-desafios/13 - calculadora_geometrica/script.js
@@ -0,0 +1,22 @@
+let choice = ''
+
+function areaDoTriângolo() {
+
+}
+function areaDoRetangolo() {
+    
+}
+function areaDoQuadrado() {
+    
+}
+function areaDoTrapezio() {
+    
+}
+function areaDoCirculo () {
+    
+}
+
+do {
+    choice = prompt("ESCOLHA UM CALCULO GEOMETRICO \n\n1. Calcular área do triângolo \n2. Calcular área do retângolo \n3. Calcular área do quadrado \n4. Calcular área do trápezio \n5. Calcular área do círculo \n6. SAIR")
+
+} while(choice !== '6')
