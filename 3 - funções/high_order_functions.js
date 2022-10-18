35ac837b8f690b0c62a9cf1eb09b13abd9418b13
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-18 14:04:18 -0300

    mais

--- 3 - funções/high_order_functions.js
+++ 3 - funções/high_order_functions.js
@@ -22,3 +22,23 @@ function chamadaMaior(n1, n2, somar) {
 chamadaMaior(5, 5, function (num1, num2) {
     console.log(num1 + num2)
 })
+
+
+
+// MOSTRANDO ELEMENTOS DE UMA LISTA [FORMA MANUAL]
+function mostrarElemento(elemento, indice, array) {
+    let obj = {
+        elemento: elemento,
+        indice: indice,
+        array: array
+    }
+    console.log(obj)
+}
+
+let arr = ['Maça', 'Laranja', 'Melão', 'Uva'] 
+
+for (let i = 0; i<arr.length; i++) {
+    mostrarElemento(arr[i], i, arr)
+}
+
+//=======OU  arr.forEach(mostrarElemento) =======//
