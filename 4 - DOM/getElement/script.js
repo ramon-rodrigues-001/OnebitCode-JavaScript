bfa5ba4e332163dda485988a4718ebde2d9a3d00
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-23 11:23:21 -0300

    UEE

---
+++ 4 - DOM/getElement/script.js
@@ -0,0 +1,11 @@
+// Para selecionar um elemento expecifico
+let elemento1 = document.getElementById('id-Do-Elemento')
+// OU
+let elemento2 = document.querySelector('teg#id > filho')
+
+// Para selecionar varios elementos
+let elemento3 = document.getElementsByTagName('li')
+// OU
+let elemento4 = document.getElementsByClassName('class-Do-Elemento')
+// OU
+let elemento5 = document.querySelectorAll('.class > filho > filho')
