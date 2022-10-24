5faf13c0654c1f9ed288128c92cefa4aee89cf5a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-24 19:05:19 -0300

    fim dia

--- 999 - pequenos-desafios/16 - escalando_time/script.js
+++ 999 - pequenos-desafios/16 - escalando_time/script.js
@@ -1,2 +1,34 @@
-const corpo = document.getElementsByTagName('body')[0]
-corpo.innerHTML = '<h1>Escalação do Time</h1> <hr>'
+let divTime = document.querySelector('div#time')
+let lista_ul;
+let lista_li;
+let arr = []
+
+function adicionar() {
+    let posicao = document.querySelector('input#posicao').value
+    let nome = document.querySelector('input#nome').value
+    let numero = document.querySelector('input#numero').value
+
+    let obj = {posicao, nome, numero}
+    arr.push(`${obj.posicao} - ${obj.nome} - Camisa:${obj.numero}`)
+
+    let lista_ul = document.createElement('ul')
+    let lista_li = document.createElement('li')
+    lista_li.innerText = arr[arr.length -1]
+
+    lista_ul.appendChild(lista_li)
+    divTime.appendChild(lista_ul)
+}
+
+function remover() {
+    let removerJogador = document.querySelector('input#remover').value
+
+    for (let i = 0; i<arr.length; i++) {
+        if (arr[i].numero === removerJogador) {
+            arr.splice(arr[i], 1)
+            
+            lista_li.innerText = arr[arr.length -1]
+            lista_ul.appendChild(lista_li)
+            divTime.appendChild(lista_ul)
+        }
+    }
+}
