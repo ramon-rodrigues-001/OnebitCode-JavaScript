bc3ba16341f85b57445c585d60c1cb777dce376b
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-26 11:26:16 -0300

    Fim do exercício de time

--- 999 - pequenos-desafios/16 - escalando_time/script.js
+++ 999 - pequenos-desafios/16 - escalando_time/script.js
@@ -1,34 +1,35 @@
-let divTime = document.querySelector('div#time')
-let lista_ul;
-let lista_li;
-let arr = []
+let posicao;
+let nome;
+let numero;
 
 function adicionar() {
-    let posicao = document.querySelector('input#posicao').value
-    let nome = document.querySelector('input#nome').value
-    let numero = document.querySelector('input#numero').value
+    posicao = document.querySelector('input#posicao').value
+    nome = document.querySelector('input#nome').value
+    numero = document.querySelector('input#numero').value    
 
-    let obj = {posicao, nome, numero}
-    arr.push(`${obj.posicao} - ${obj.nome} - Camisa:${obj.numero}`)
+    let confirmar = confirm(`Escalar ${nome} na posição de ${posicao}?`)
+    if (confirmar) {
+        let lista_ul = document.querySelector('ul#lista_ul')
+        let lista_li = document.createElement('li')
+        lista_li.innerHTML = `${posicao}: ${nome} - Camisa: ${numero}`
+        lista_li.id = `camisa${numero}`
 
-    let lista_ul = document.createElement('ul')
-    let lista_li = document.createElement('li')
-    lista_li.innerText = arr[arr.length -1]
+        lista_ul.appendChild(lista_li)
 
-    lista_ul.appendChild(lista_li)
-    divTime.appendChild(lista_ul)
+        document.querySelector('input#posicao').value = ''
+        document.querySelector('input#nome').value = ''
+        document.querySelector('input#numero').value = ''
+    }
+
 }
 
 function remover() {
     let removerJogador = document.querySelector('input#remover').value
+    let removeItemli = document.getElementById(`camisa${removerJogador}`)
 
-    for (let i = 0; i<arr.length; i++) {
-        if (arr[i].numero === removerJogador) {
-            arr.splice(arr[i], 1)
-            
-            lista_li.innerText = arr[arr.length -1]
-            lista_ul.appendChild(lista_li)
-            divTime.appendChild(lista_ul)
-        }
+    let confirmar = confirm(`Deseja remover jogador camisa: ${removerJogador} ?`)
+    if (confirmar) {
+        document.querySelector('ul#lista_ul').removeChild(removeItemli)
+        document.querySelector('input#remover').value = ''
     }
 }
