96fbc2c11028ae017d98d34605d0c125b1fa9f5f
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-27 14:03:26 -0300

    ainda não

--- 999 - pequenos-desafios/17 - cadastrando_devs_Form/script.js
+++ 999 - pequenos-desafios/17 - cadastrando_devs_Form/script.js
@@ -1,8 +1,10 @@
-const form = document.getElementById('formulario')
-const habilidade = document.getElementById('habilidades')
+let lista_li;
 
 const addHabilidade = document.getElementById('adicionar')
 addHabilidade.addEventListener('click', () => {
+    const habilidade = document.getElementById('habilidades')
+    lista_li = document.createElement('li')
+
     let labelFeramenta = document.createElement('label')
     labelFeramenta.for = 'feramenta'
     labelFeramenta.innerText = 'Ferramenta: '
@@ -11,16 +13,20 @@ addHabilidade.addEventListener('click', () => {
     feramenta.type = 'text'
     feramenta.id = 'feramenta'
 
+    labelFeramenta.appendChild(feramenta)
+    lista_li.appendChild(labelFeramenta)
 
+
+    
     let labelExperiencia1 = document.createElement('label')
     let labelExperiencia2 = document.createElement('label')
     let labelExperiencia3 = document.createElement('label')
-    labelExperiencia1.for = 'experiencia1'
-    labelExperiencia2.for = 'experiencia2'
-    labelExperiencia3.for = 'experiencia3'    
     labelExperiencia1.innerText = '0-2 Anos' 
     labelExperiencia2.innerText = '3-4 Anos' 
     labelExperiencia3.innerText = '5+ Anos'
+    labelExperiencia1.for = 'experiencia1'
+    labelExperiencia2.for = 'experiencia2'
+    labelExperiencia3.for = 'experiencia3'
 
     let experiencia1 = document.createElement('input')
     let experiencia2 = document.createElement('input')
@@ -28,18 +34,21 @@ addHabilidade.addEventListener('click', () => {
     experiencia1.type = 'radio'
     experiencia2.type = 'radio'
     experiencia3.type = 'radio'
+    experiencia1.name = 'inputRadio'
+    experiencia2.name = 'inputRadio'
+    experiencia3.name = 'inputRadio'
     experiencia1.id = 'experiencia1'
     experiencia2.id = 'experiencia2'
     experiencia3.id = 'experiencia3'
 
-
-    labelFeramenta.appendChild(feramenta)
-    habilidade.appendChild(labelFeramenta)
-
     labelExperiencia1.appendChild(experiencia1)
     labelExperiencia2.appendChild(experiencia2)
     labelExperiencia3.appendChild(experiencia3)
-    habilidade.appendChild(labelExperiencia1)
-    habilidade.appendChild(labelExperiencia2)
-    habilidade.appendChild(labelExperiencia3)
+
+    lista_li.appendChild(labelExperiencia1)
+    lista_li.appendChild(labelExperiencia2)
+    lista_li.appendChild(experiencia3)
+    lista_li.appendChild(labelExperiencia3)
+
+    habilidade.appendChild(lista_li)
 })
