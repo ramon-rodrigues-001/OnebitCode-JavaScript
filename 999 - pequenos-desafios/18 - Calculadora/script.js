c5b157d072ffdc7212231fddf6294ebd50b9c77f
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-10-31 19:40:25 -0300

    Fim do dia

--- 999 - pequenos-desafios/18 - Calculadora/script.js
+++ 999 - pequenos-desafios/18 - Calculadora/script.js
@@ -5,12 +5,23 @@ document.getElementById('mudarTema').addEventListener('click', ()=>{
 })
 
 let tela = document.getElementById('tela')
+let telaDeResultado = document.getElementById('telaDeResposta')
+let calculo = []
+let res;
 
 document.querySelectorAll('.teclas').forEach((el)=>{
     el.addEventListener('click', (evento)=>{
 
         let elementoAcionado = evento.currentTarget
 
+        calculo.push(elementoAcionado.value)
+
         tela.value += `${elementoAcionado.value}`
     })
+})
+
+let sosd = []
+document.getElementById('efetuarCalculo').addEventListener('click', ()=>{
+
+    console.log(calculo)
 })
