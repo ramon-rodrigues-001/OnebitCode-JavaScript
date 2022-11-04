e634191fa952c0b253462a9675b3e7cd5eda7f6a
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-11-04 13:24:50 -0300

    atualizando

--- 999 - pequenos-desafios/18 - jogo_da_velha/script.js
+++ 999 - pequenos-desafios/18 - jogo_da_velha/script.js
@@ -1,33 +1,67 @@
 
 let numJugada = 0
+let jogadorX;
+let jogadorO;
+
+function MudarJogador() {
+    const vezDoJogador = document.getElementById('vezDoJogador')
+    
+    if (vezDoJogador.innerText === 'Jogador' || vezDoJogador.innerText === jogadorO.value) {
+        vezDoJogador.innerText = jogadorX.value
+    }
+    else if (vezDoJogador.innerText === jogadorX.value) {
+        vezDoJogador.innerText = jogadorO.value
+    }
+
+    return
+}
 
 document.getElementById('btnComecar').addEventListener('click', (evComecar)=>{
-    const jogadorX = document.getElementById('jogadorX')
-    const jogadorO = document.getElementById('jogadorO')
+    
+    let espacos = document.querySelectorAll('.marcado').forEach(element =>{
+        element.classList.remove('marcado')
+        element.classList.add('espaco')
+        element.innerText = ''
+        numJugada = 0
+    })
+
+    jogadorX = document.getElementById('jogadorX')
+    jogadorO = document.getElementById('jogadorO')
+    
+    MudarJogador()
+
 
     if (jogadorX.value !== '' && jogadorO.value !== '') {
         alert('Lest Go!')
-        document.querySelectorAll('.espaco').forEach(element => {
 
-            element.classList.remove('espaco')
-            element.classList.add('marcado')
-
-            if ()
-
+        espacos = document.querySelectorAll('.espaco').forEach(element => {
             element.addEventListener('click', (eventoClicar)=>{
                 const espacoClicado = eventoClicar.currentTarget
 
-                if (numJugada % 2 === 0) {
-                    espacoClicado.innerText = 'X'
-                } 
-                else {
-                    espacoClicado.innerText = 'O'
+                if (espacoClicado.classList == 'espaco') {
+                    if (numJugada % 2 === 0) {
+                        espacoClicado.innerText = 'X'
+                        element.classList.remove('espaco')
+                        element.classList.add('marcado')
+                    } 
+                    else {
+                        
+                        espacoClicado.innerText = 'O'
+                        element.classList.remove('espaco')
+                        element.classList.add('marcado')
+                    }
+                    
+                    MudarJogador()
+                    numJugada += 1
                 }
-                numJugada += 1
             })
+
+
         });
     }
     else {
         alert('Favor preencher campos de nome')
     }
+
+
 })
