b0b0d9b9d9b8e8968932faf594a34ddcaacb65f7
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-11-10 10:31:12 -0300

    exercício completo

---
+++ 999 - pequenos-desafios/19 - 4_exercicios/media-ponderada.js
@@ -0,0 +1,17 @@
+function medPonderada(...objs) {
+    console.log(objs)
+
+    let pesos = 0
+    let total = 0
+
+    objs.map((elemento)=>{
+        total = total + (elemento.p * elemento.m)
+        pesos += elemento.p
+    })
+
+    let media = total / pesos
+
+    console.log(`A media ponderada foi de ${media}`)
+}
+
+medPonderada({m:7, p:2}, {m:9, p:5}, {m:3, p:1})
