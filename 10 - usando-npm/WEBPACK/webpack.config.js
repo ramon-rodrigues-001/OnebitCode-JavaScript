56bf86b9971a2bd7e8935c22c049d81dbfc018a8
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-11-17 17:23:42 -0300

    h

---
+++ 10 - usando-npm/WEBPACK/webpack.config.js
@@ -0,0 +1,24 @@
+// [entry] = O caminho até a pasta que queremos pegar esse caminho começa desta pasta webpack.config.js pode pegar mais de um arquivo se especificar o nome dele no final
+
+// [mode] = O modo em que deseja fazer o codigo:
+// [development] = Um modo onde o codigo não fica compacto para que ainda de para o desenvolvedor entender oque esta acontecendo
+// [production] = Extremamente compacto
+
+// [output] = A saida, você pode diser onde que que este arquivo compacto sejá criado e tambem qual vai ser o nome dele [se o output não for criado o nome vai ser a chave pasada no [entry] UTILIZE UM MODULO PARA PEGAR O CAMINHO CORRETAMENTE]
+
+
+const url = require('path')
+
+module.exports = {
+    entry: {
+        index: "./src/index.js",
+        hello: "./src/hello-world.js"
+    },
+
+    mode: 'development',
+
+    output: {
+        path: url.resolve(__dirname, 'AquivoCriado'),
+        filename: "[name].ArquivoCompacto.js"
+    }
+}
