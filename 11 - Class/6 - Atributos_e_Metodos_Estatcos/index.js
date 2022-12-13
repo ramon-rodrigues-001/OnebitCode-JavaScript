e33603be6d92f1187cb21e081e8bf36eb13db7c3
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-12-13 17:22:18 -0300

    Atributos e métodos estáticos

---
+++ 11 - Class/6 - Atributos_e_Metodos_Estatcos/index.js
@@ -0,0 +1,34 @@
+
+// Apartamento
+// O elemento com Static sera passado para class filha exatamente com o valor adiquirido
+
+
+class Reservation {
+    constructor(hospedes, quarto, day) {
+        this.hospedes = hospedes
+        this.quarto = quarto
+        this.day = day
+        this.total = day * Reservation.diaria
+    }
+
+    static diaria = 150
+
+    static get lerDiaria() {
+        console.log(`O valor diario é ${this.diaria}`)
+    }
+}
+Reservation.lerDiaria
+const reser1 = new Reservation(4, 2, 7)
+console.log(reser1)
+
+
+// Instancia de Reservation
+class Primium extends Reservation {
+    constructor(hospedes, quarto, day) {
+        super(hospedes, quarto, day)
+        this.total *= 1.5
+    }
+}
+Primium.lerDiaria
+const reser2 = new Primium(4, 2, 7)
+console.log(reser2)
