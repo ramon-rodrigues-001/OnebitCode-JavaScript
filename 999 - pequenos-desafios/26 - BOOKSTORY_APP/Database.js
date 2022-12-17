b4797a2cc1cc99168b4c97af895c5d11893445b4
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-12-17 20:14:09 -0300

    Update Database

--- 999 - pequenos-desafios/26 - BOOKSTORY_APP/Database.js
+++ 999 - pequenos-desafios/26 - BOOKSTORY_APP/Database.js
@@ -6,4 +6,22 @@ class Database {
         ouders: [],
         usser: []
     }
+
+    find(key) {
+        return this.#storage[key]
+    }
+
+    saveAuthor(author) {
+        this.#storage.authores.push(author)
+    }
+
+    saveBook(Book) {
+        this.#storage.books.push(Book)
+    }
+
+    savePoster(poster) {
+        this.#storage.posters.push(poster)
+    }
+
+    save
 }
