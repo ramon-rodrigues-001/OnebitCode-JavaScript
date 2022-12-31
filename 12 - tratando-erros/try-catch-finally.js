85ace86abb5765ebef314f3f9e344d3b50aa9e8d
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2022-12-31 10:34:03 -0300

    Try Catch finally

--- 12 - tratando-erros/try-catch-finally.js
+++ 12 - tratando-erros/try-catch-finally.js
@@ -1,29 +1,25 @@
 function soma(a, b) {
     const num1 = Number(a)
     const num2 = Number(b)
-
+    
     if (isNaN(num1) || isNaN(num2)) {
-        throw new Error('Ambos os elementos devem ser numeros.')
+        throw new Error("Os elementos sugeridos dever ser numeros.")
     }
+
     return num1 + num2
 }
 
-
-// Tente execultar esse bloco de codigo
 try {
-    console.log(soma('5', 5))
-    console.log(soma(3, true))
-    console.log(soma('zero', 8))
-    console.log(soma(9, 1))
+    console.log(soma(2, 6))
+    console.log(soma(true, 6))
+    console.log(soma('8', 9))
+    console.log(soma('zero', 5))
+    console.log(soma(2, 5))
 }
-
-//  Caso algo de errado mostre estas mensagens
-catch(error) {
-    console.log('An error ocurred!!')
+catch (error) {
+    console.log("An error ocurred!")
     console.log(error.message)
 }
-
-// Por fim execulte isso
 finally {
-    console.log('Calculations finished!')
+    console.log('Calculations finished.')
 }
