0eb5fdfae11d36dbbbc5ae0793a62a7b2cb3c78b
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-12 18:46:20 -0300

    nd

--- 999 - pequenos-desafios/30 - Copia-brizzard/Script/MudarBackground.js
+++ 999 - pequenos-desafios/30 - Copia-brizzard/Script/MudarBackground.js
@@ -1,1 +1,19 @@
-class 
+export class MudarBackground {
+    constructor (elementos, elementClicado) {
+        this.elementos = elementos
+        this.elementClicado = elementClicado
+    }
+
+    mudar() {
+        this.elementos.forEach((e)=> {
+            e.classList.remove('ativo')
+        })
+
+        this.elementClicado.classList.add('ativo')
+
+        const section_1 = document.querySelector('#section-1')
+        section_1.style.transition = '.5s'
+        section_1.style.backgroundImage = `url(${this.elementClicado.dataset.url})`
+        
+    }
+}
--- 999 - pequenos-desafios/30 - Copia-brizzard/Script/index.js
+++ 999 - pequenos-desafios/30 - Copia-brizzard/Script/index.js
@@ -1,26 +1,12 @@
+import { MudarBackground } from "./MudarBackground.js";
+
 let elementos = document.querySelectorAll('img.circles')
 
 elementos.forEach(element => {
     element.addEventListener('click', (E) => {
         let elementClicado = E.currentTarget
 
-        funcao(elementos, elementClicado)
+        const section_1 = new MudarBackground(elementos, elementClicado)
+        section_1.mudar()
     })
 });
-
-
-
-
-function funcao(elementos, elementClicado) {
-    elementos.forEach((e)=> {
-        e.classList.remove('ativo')
-    })
-
-    elementClicado.classList.add('ativo')
-
-    const section_1 = document.querySelector('#section-1')
-
-    section_1.style.backgroundImage = `url(${elementClicado.dataset.url})`
-
-    console.log(elementClicado.dataset.url)
-}
