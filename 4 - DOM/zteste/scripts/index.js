408d490ee81dab1c64ccff7f329828d945e1ed78
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-19 09:23:50 -0300

    Armarzenando dados

--- 4 - DOM/zteste/scripts/index.js
+++ 4 - DOM/zteste/scripts/index.js
@@ -3,10 +3,12 @@ import { acionar } from "./manipulando-atributos.js"
 import { evento } from "./eventos.js"
 import { pedir } from "./formulario.js";
 
+// Addicionar inputs no Body
 document.querySelector('button#chamada_add').addEventListener('click', () => {
     add()
 })
 
+// Eventos mudando um input
 let btns = document.querySelectorAll('.btns').forEach(element => {
     element.addEventListener('click', (e) => {
         let element_clicado = e.currentTarget
@@ -16,13 +18,14 @@ let btns = document.querySelectorAll('.btns').forEach(element => {
 });
 
 
+// Login
 document.querySelector('#input_submit').addEventListener('click', (e)=> {
     e.preventDefault()
     let element_pai = e.currentTarget.parentNode
     evento(element_pai)
 })
 
-
+// Pedido em um formulario
 document.querySelector('#pedir').addEventListener('click', (e2)=> {
     e2.preventDefault()
     let element = e2.currentTarget.parentNode
