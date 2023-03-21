6e70c51b42caa74c63285ba793334e594f626695
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-21 19:37:34 -0300

    Notas

---
+++ 999 - pequenos-desafios/31 - Agenda/Script/index.js
@@ -0,0 +1,5 @@
+document.querySelector('#confirm').addEventListener('click', ()=> {
+    let title = document.querySelector('#title').value
+    let text = document.querySelector('#text').value
+    console.log(title, text)
+})
