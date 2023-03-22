20c176ec7f60c5106b3b8581767ea1c9a8498d55
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-22 20:23:03 -0300

    fim do dia

--- 999 - pequenos-desafios/31 - Agenda/Script/nota.js
+++ 999 - pequenos-desafios/31 - Agenda/Script/nota.js
@@ -16,18 +16,8 @@ export class Notas {
         div.style.padding = '10px'
         div.style.overflow = 'hidden'
 
-        const data = document.createElement('h4')
-        data.innerText = new Date().getDate() + ' / '
-        if (new Date().getMonth()+1 < 10) {
-            data.innerText += '0' + new Date().getMonth() + ' / '
-        }
-        else {
-            data.innerText += new Date().getMonth() + ' / '
-        }
-        data.innerText += new Date().getFullYear()
 
-
-
+// Configuração do titulo do bloco com limitador de texto
         const h1 = document.createElement('h1')
         let letras_title = ''
         if (this.titulo.length > 8) {
@@ -44,7 +34,35 @@ export class Notas {
         }
 
 
+// Configurando Data e Hora
+        const data = document.createElement('h4')
+        data.innerText = new Date().getDate() + ' / '
+        if (new Date().getMonth()+1 < 10) {
+            data.innerText += '0' + new Date().getMonth() + ' / '
+        }
+        else {
+            data.innerText += new Date().getMonth() + ' / '
+        }
+        data.innerText += new Date().getFullYear()
 
+        const hora = document.createElement('p')
+        if (new Date().getHours < 10) {
+            hora.innerText = '0' + new Date().getHours() + ':'
+        }
+        else {
+            hora.innerText = new Date().getHours() + ':'
+        }
+        if (new Date().getMinutes < 10) {
+            hora.innerText += '0' + new Date().getMinutes()
+        }
+        else {
+            hora.innerText += new Date().getMinutes()
+        }
+
+
+
+        
+// Configuração do texto do bloco com limitador de texto
         const p = document.createElement('p')
         let letras_text = ''
         if (this.texto.length > 73) {
@@ -62,20 +80,53 @@ export class Notas {
 
 
 
+// Criando o graus de importancia
+        const p_label = document.createElement('p')
+        p_label.innerText = 'Importancia: '
+        p_label.style.display = 'inline'
+        
+        const label_1 = document.createElement('label')
+        label_1.innerText = '1°'
+        label_1.style.marginLeft = '10px'
+        const input_1 = document.createElement('input')
+        input_1.type = 'radio'
+        input_1.id = 'radio_1'
+        input_1.name = 'importancia'
 
+        const label_2 = document.createElement('label')
+        label_2.innerText = '2°'
+        label_2.style.marginLeft = '10px'
+        const input_2 = document.createElement('input')
+        input_2.type = 'radio'
+        input_1.id = 'radio_2'
+        input_2.name = 'importancia'
+
+        const label_3 = document.createElement('label')
+        label_3.innerText = '3°'
+        label_3.style.marginLeft = '10px'
+        const input_3 = document.createElement('input')
+        input_3.type = 'radio'
+        input_1.id = 'radio_3'
+        input_3.name = 'importancia'
         
 
+
+
+// Adicionando os elementos no bloco [div]
         div.appendChild(h1)
         div.appendChild(data)
+        div.appendChild(hora)
         div.appendChild(p)
 
+        div.appendChild(p_label)
+        div.appendChild(label_1)
+        div.appendChild(input_1)
+        div.appendChild(label_2)
+        div.appendChild(input_2)
+        div.appendChild(label_3)
+        div.appendChild(input_3)
+
+// Adicionando o bloco [div] no main
         this.main.appendChild(div)
     }
 }
-
-// position: relative;
-//         background-color: rgb(214, 210, 210);
-//         display: flex;
-//         justify-content: center;
-//         align-items: center;
-//         flex-direction: column;
