be8f6bd26a3e0bf0a8908bde30d5054b370fe813
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-22 18:48:48 -0300

    Notas

--- 999 - pequenos-desafios/31 - Agenda/Script/nota.js
+++ 999 - pequenos-desafios/31 - Agenda/Script/nota.js
@@ -13,16 +13,34 @@ export class Notas {
         div.style.textAlign = 'center'
         div.style.borderRadius = '5px'
         div.style.boxShadow = '2px 2px 2px rgba(0, 0, 0, 0.28)'
+        div.style.padding = '10px'
+        div.style.overflow = 'hidden'
+
+        const data = document.createElement('h4')
+        data.innerText = new Date().getDate() + ' / '
+        if (new Date().getMonth()+1 < 10) {
+            data.innerText += '0' + new Date().getMonth() + ' / '
+        }
+        else {
+            data.innerText += new Date().getMonth() + ' / '
+        }
+        data.innerText += new Date().getFullYear()
 
         const h1 = document.createElement('h1')
+        if (this.titulo.lengft > 8) {
+            h1.innerText = this.titulo.fo
+        }
         h1.innerText = this.titulo
 
         const p = document.createElement('p')
         p.innerText = this.texto
 
+        let tetttt = 'Rap do Roy Mustang (Fullmetal Alchemist) | Alquimista das Chamas | Enygma 71'
 
         div.appendChild(h1)
+        div.appendChild(data)
         div.appendChild(p)
+
         this.main.appendChild(div)
     }
 }
