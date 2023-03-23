a966d67576c8192554677b55eef68d5702f2f7b7
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-03-23 19:36:35 -0300

    Sem resultados relevantes com salvamento

--- 999 - pequenos-desafios/31 - Agenda/Script/index.js
+++ 999 - pequenos-desafios/31 - Agenda/Script/index.js
@@ -1,20 +1,21 @@
 import { Notas } from "./nota.js"
-
-let storage = []
+let i = 0
 document.querySelector('#confirm').addEventListener('click', ()=> {
     let title = document.querySelector('#title').value
     let text = document.querySelector('#text').value
     const newBloco = new Notas(title, text)
-    storage.push(newBloco)
+    console.log(Notas.titulo)
     newBloco.criar()
 
 
     const index = window.localStorage.length
-    window.localStorage.setItem(index, storage)
+    window.localStorage.setItem(index, `${Notas.titulo} ${Notas.texto}`)
     alert(index)
+    
 
-    window.localStorage.getItem(index).array.forEach(element => {
-        console.log(element)
-    })
+    while (index > i) {
+        console.log(window.localStorage.getItem(index))
+        i++
+    }
 })
 
