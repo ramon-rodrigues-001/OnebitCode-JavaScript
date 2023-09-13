1ce04b1b38417c2fe905cff57cf14dfe97902d11
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-09-13 20:16:43 -0300

    Project success

--- 999 - EXERCICIOS/10.2 - fila_de_espera/index.js
+++ 999 - EXERCICIOS/10.2 - fila_de_espera/index.js
@@ -1,8 +1,41 @@
 const fila = document.querySelector('#divFila')
+const emAtendimento = document.querySelector('#divAtendente')
+const Pacientes = []
 
-function addPaciente(nome) {
-    const newPaciente = document.createElement('p')
-    newPaciente.innerHTML = nome
+function addPaciente() {
+    const nome = prompt('Nome do Paciente: ')
+    Pacientes.push(nome)
+    fila.innerHTML = ''
 
-    fila.addChilden(newPaciente)
+    let index = 0
+    Pacientes.forEach(value => {
+        index ++
+        const newParagrafh = document.createElement('p')
+        newParagrafh.innerHTML = `${index}° ${value}`
+
+        fila.appendChild(newParagrafh)
+    })
+}
+
+
+function Atender() {
+    const pacienteAtendido = Pacientes.shift()
+    emAtendimento.innerHTML = ''
+    fila.innerHTML = ''
+
+    const newParagrafh = document.createElement('p')
+    newParagrafh.innerHTML = pacienteAtendido
+
+    emAtendimento.appendChild(newParagrafh)
+
+
+    // Renderizando novamente a lista de paciente em espera
+    let index = 0
+    Pacientes.forEach(value => {
+        index ++
+        const newParagrafh = document.createElement('p')
+        newParagrafh.innerHTML = `${index}° ${value}`
+
+        fila.appendChild(newParagrafh)
+    })
 }
