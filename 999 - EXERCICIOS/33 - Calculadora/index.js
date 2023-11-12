db518eb795dd0a3c0c9236a645c216949678b36c
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-11-12 20:10:49 -0300

    projeto completo

--- 999 - EXERCICIOS/33 - Calculadora/index.js
+++ 999 - EXERCICIOS/33 - Calculadora/index.js
@@ -6,10 +6,38 @@ document.querySelectorAll('.buttons').forEach(element => {
     element.addEventListener('click', (event)=> {
         const valor = event.currentTarget.value
         tela.value += valor
+
+        mudarBtnCopia()
     })
 })
 
 
 function apagarUm() {
     tela.value = tela.value.slice(0, -1)
+}
+
+
+function calcular() {
+    try {
+        if (tela.value === '') {
+            telaResultado.value = 'Calcule algo'
+        }
+        else {
+            telaResultado.value = eval(tela.value)
+        }
+    }
+    catch {
+        telaResultado.value = 'Error'
+    }
+}
+
+
+const btnCopy = document.querySelector(".copy")
+function copiar() {
+    btnCopy.innerHTML = 'COPYED'
+    window.navigator.clipboard.writeText(telaResultado.value)
+}
+
+function mudarBtnCopia() {
+    btnCopy.innerHTML = 'COPY'
 }
