c0c74e25f4dc4505e4bedb5bb077debc9c0d9c50
ramon-rodrigues-001 <109005234+ramon-rodrigues-001@users.noreply.github.com>
2023-11-26 17:00:11 -0300

    adaptando novas tecnologias

---
+++ 999 - EXERCICIOS/34 - Apiario_abelha/front/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.scss'
+import AlertTop from './components/common/AlertTop/AlertTop'
+
+function App() {
+  return (
+    <>
+      < AlertTop />
+
+      <header>
+        <img src="/icon_bee_2.png" alt="" />
+
+        <ul>
+          li
+        </ul>
+      </header>
+    </>
+  )
+}
+
+export default App
---
+++ 999 - EXERCICIOS/34 - Apiario_abelha/front/src/components/common/AlertTop/AlertTop.jsx
@@ -0,0 +1,9 @@
+import './AlertTop.scss'
+
+export default function AlertTop() {
+    return (
+        <div id="alertTop">
+            <p>Entrega imediata para toda Capelinha MG, e região.</p>
+        </div>
+    )
+}
